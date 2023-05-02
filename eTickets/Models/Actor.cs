6b610620459af66f0f3cb8a42b6163901c3a32d7
Pulkit Anand <pulkitanand@Pulkit-Anands-MacBook-Pro.local>
2023-05-02 15:58:18 +0530

    done with relational model with joins

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -12,6 +12,9 @@ namespace eTickets.Models
         public string ProfilePictureUrl { get; set; }="";
         public string FullName { get; set; }="";
         public string Bio { get; set; }="";
+        
+        //Relationships
+        public  List<Actor_Movie>? Actors_Movies { get; set; }
 
     }
 }
