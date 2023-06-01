62c1f0c8aa158d689d076b23f47a2385ad175b3d
Pulkit Anand <pulkitanand@Pulkit-Anands-MacBook-Pro.local>
2023-06-01 21:10:11 +0530

    done with order without paypal

---
+++ eTickets/Migrations/20230601153839_Order_And_OrderItem_Added.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eTickets.Migrations
+{
+    /// <inheritdoc />
+    public partial class Order_And_OrderItem_Added : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_Movies_MovieId1",
+                table: "OrderItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItems_MovieId1",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "MovieId1",
+                table: "OrderItems");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Orders",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserEmailAddress",
+                table: "Orders",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MovieId",
+                table: "OrderItems",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_MovieId",
+                table: "OrderItems",
+                column: "MovieId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_Movies_MovieId",
+                table: "OrderItems",
+                column: "MovieId",
+                principalTable: "Movies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_Movies_MovieId",
+                table: "OrderItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItems_MovieId",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "UserEmailAddress",
+                table: "Orders");
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "UserId",
+                keyValue: null,
+                column: "UserId",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Orders",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MovieId",
+                table: "OrderItems",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MovieId1",
+                table: "OrderItems",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_MovieId1",
+                table: "OrderItems",
+                column: "MovieId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_Movies_MovieId1",
+                table: "OrderItems",
+                column: "MovieId1",
+                principalTable: "Movies",
+                principalColumn: "Id");
+        }
+    }
+}
